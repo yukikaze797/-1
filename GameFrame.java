2620ae9bfd15f5706aa5bc9a7846d1d9bc5f9bec
yukikaze797 <133753386+yukikaze797@users.noreply.github.com>
2023-05-23 09:02:59 +0800

    GameFrame.java

--- GameFrame.java
+++ GameFrame.java
@@ -6,10 +6,12 @@ import java.awt.event.ActionListener;
 import Model.Animal.Person;
 import Model.Chess.CellType;
 import Model.Chess.ChessBoard;
+//游戏总框架
 
 public class GameFrame extends JFrame {
+    //检测存档
     private int saveTest;
-    public ChessBoard save = new ChessBoard(new Person(-1),new Person(1));;//已经有的存档,使用默认存档
+    public ChessBoard save = new ChessBoard(new Person(-1),new Person(1));;//已经有的存档,新游戏使用默认存档
     public ChessBoard oriSave = new ChessBoard(new Person(-1),new Person(1));//默认存档
 
     public ChessBoard getOriSave() {
@@ -36,7 +38,7 @@ public class GameFrame extends JFrame {
         this.save = save;
     }
 
-    //游戏最大的结构的构造器
+    //构造器
     public GameFrame(ChessBoard chessBoard) {
         JFrame GameBoard = new JFrame("斗兽棋");
         GameBoard.setSize(800, 800);
@@ -57,7 +59,7 @@ public class GameFrame extends JFrame {
             saveTest = 1;
         }
     }
-
+//主菜单
     public static class MainMenu extends JPanel {
         private JButton Start = new JButton("开始游戏");
         private JButton Exit = new JButton("退出游戏");
@@ -65,7 +67,7 @@ public class GameFrame extends JFrame {
         private JButton Load = new JButton("读取存档");
 
 
-        //具体的主菜单 0为无存档时的界面，1为有存档时的界面
+        //具体的主菜单 0为无玩家存档时的界面，1为有存档时的界面
         public MainMenu(int number) {
             if (number == 0) {
                 this.add(Start);
@@ -89,20 +91,23 @@ public class GameFrame extends JFrame {
                 Exit.setActionCommand("exit");
             }
         }
-
+//按钮事件监听器，还没写完
         public class ButtonListener implements ActionListener {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String command = e.getActionCommand();
                 switch (command) {
                     case "start":
+                        //打开游戏窗口
                         JFrame playFrame = new PlayFrame(new GameFrame().oriSave);
+                        //关闭主菜单
                         Window windowS = SwingUtilities.getWindowAncestor(MainMenu.this);
                         if (windowS instanceof JFrame) {
                             JFrame frame = (JFrame) windowS;
                             frame.dispose();
                         }
                     case "exit":
+                        //关闭游戏
                         Window windowE = SwingUtilities.getWindowAncestor(MainMenu.this);
                         if (windowE instanceof JFrame) {
                             JFrame frame = (JFrame) windowE;
