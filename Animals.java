33193d4fc6805cb53a76e8d8bca154f62e3faaf4
yukikaze797 <133753386+yukikaze797@users.noreply.github.com>
2023-05-23 08:02:18 +0800

    Animals.java

--- Animals.java
+++ Animals.java
@@ -2,6 +2,7 @@ package Model.Animal;
 
 public class Animals {
     private int rank;
+    private String name;
 
     public int getRank() {
         return rank;
@@ -9,5 +10,16 @@ public class Animals {
 
     public void setRank(int rank) {
         this.rank = rank;
+    }
+     public String getName() {
+        if(this == null){
+            return "null";
+        }else{
+        return name;
+        }
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
